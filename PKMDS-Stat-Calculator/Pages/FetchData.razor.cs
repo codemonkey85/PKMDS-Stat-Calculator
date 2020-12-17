5f891fc8396629e3a6054d63923ce70e00ae4515
Michael Bond <mbond@3storysoftware.com>
2020-12-17 08:29:47 -0500

    Add selection to choose which Pokemon to query

--- PKMDS-Stat-Calculator/Pages/FetchData.razor.cs
+++ PKMDS-Stat-Calculator/Pages/FetchData.razor.cs
@@ -1,19 +1,34 @@
 ﻿using PokemonApiHelper;
 using PokemonApiHelper.Models.Pokemon;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace PKMDS_Stat_Calculator.Pages
 {
     public partial class FetchData
     {
-        private readonly IList<Pokemon> pokemonList = new List<Pokemon>();
+        private IList<Pokemon> pokemonList = new List<Pokemon>();
+
+        private IList<string> pokemonNames = new List<string>();
+
+        private string selectedPokemon = string.Empty;
 
         protected override async Task OnInitializedAsync()
         {
-            await foreach (Pokemon pokemon in PokeApiHelper.GetMultiplePokemon(5).GetAsyncEnumerator())
+            await foreach (string pokemonName in PokeApiHelper.GetAllPokemonNames())
             {
-                pokemonList.Add(pokemon);
+                pokemonNames.Add(pokemonName);
+            }
+
+            pokemonNames = pokemonNames.OrderBy(p => p).ToList();
+        }
+
+        private async Task ButtonClicked()
+        {
+            if (!string.IsNullOrEmpty(selectedPokemon))
+            {
+                pokemonList.Add(await PokeApiHelper.GetSinglePokemon(selectedPokemon));
             }
         }
     }
