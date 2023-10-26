83bae113716a5f6c873e24fceca3a1533cd93720
Michael Bond <michael@bondcodes.com>
2023-10-25 23:58:08 -0400

    Remove old project

--- PKMDS-Stat-Calculator/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using System.Text;
-global using Microsoft.AspNetCore.Components;
-global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-global using MudBlazor.Services;
-global using PKMDS_Stat_Calculator;
-global using PKMDS_Stat_Calculator.Models;
-global using PKMDS_Stat_Calculator.Shared;
-global using PokeApiNet;
